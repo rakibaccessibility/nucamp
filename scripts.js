31414fd47c6aa8f01f1b36165dbb35832194d1f2
rakibaccessibility <130183271+rakibaccessibility@users.noreply.github.com>
2023-04-07 16:21:09 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+   
+   $(function(){
+        $("#reserveButton1").click(function(){
+            $("#reserveForm").modal('show');
+        });
+    });
+
+  
+    $(function(){
+        $("#modal1").click(function(){
+            $("#loginModal").modal('show');
+        });
+    });
